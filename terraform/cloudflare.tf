d89cf777c80eef0756bc2cabffe09a205da0101e
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2023-04-12 19:56:49 -0300

    chore: update record value

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -43,6 +43,6 @@ resource "cloudflare_record" "assets_gangoffront_com" {
   proxied = true
   ttl     = 1
   type    = "CNAME"
-  value   = format("https://5df477d4f9a8cf72185ef8f44fd1e144.r2.cloudflarestorage.com/%s", var.project_name)
+  value   = "https://5df477d4f9a8cf72185ef8f44fd1e144.r2.cloudflarestorage.com"
   zone_id = var.zone_id
 }

f3fe8d5f5a1874347bd740861673720afd1551d8
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2023-05-18 19:57:24 -0300

    feat(header): add navigation

--- apps/header-app/src/root.component.tsx
+++ apps/header-app/src/root.component.tsx
@@ -1,35 +1,41 @@
+import { navigateToUrl } from 'single-spa'
+
 export default function Root() {
+  const handleNavigateClick = (url: string) => () => {
+    navigateToUrl(url)
+  }
+
   return (
     <nav className="navbar navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="index.html">
+        <a className="navbar-brand" onClick={handleNavigateClick('/')}>
           conduit
         </a>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
-            <a className="nav-link active" href="">
+            <a className="nav-link active" onClick={handleNavigateClick('/')}>
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" onClick={handleNavigateClick('/article/edit')}>
               {" "}
               <i className="ion-compose"></i>&nbsp;New Article{" "}
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" onClick={handleNavigateClick('/profile/settings')}>
               {" "}
               <i className="ion-gear-a"></i>&nbsp;Settings{" "}
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" onClick={handleNavigateClick('/profile/login')}>
               Sign in
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link" onClick={handleNavigateClick('/profile/register')}>
               Sign up
             </a>
           </li>

1361389435fbe1d9b02c41f5e58bcc9029b64326
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2024-02-16 19:14:19 -0300

    feat: wip user-flows with puppeteer replay

---
+++ .github/actions/replay/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+read -d '' COMMAND <<-EOF
+	  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
+	    echo "$BUILD_DIR didn't change"
+	  else
+	    ${BUILD_COMMAND:-echo} && npx @puppeteer/replay $*
+	  fi
+EOF
+
+OUTPUT=$(sh -c "$COMMAND")

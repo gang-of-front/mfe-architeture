d9bad64afffd11ee0c6126b934c111af2597c770
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2024-02-16 19:37:01 -0300

    chore: add chorme to docker

--- .github/actions/replay/entrypoint.sh
+++ .github/actions/replay/entrypoint.sh
@@ -4,7 +4,7 @@ read -d '' COMMAND <<-EOF
 	  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
 	    echo "$BUILD_DIR didn't change"
 	  else
-	    ${BUILD_COMMAND:-echo} && npx @puppeteer/replay $*
+	    ${BUILD_COMMAND:-echo} && npx puppeteer browsers install chrome && npx @puppeteer/replay $*
 	  fi
 EOF
 

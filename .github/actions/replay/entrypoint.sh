350429d99f5eac310fe0563187b1bc274b110702
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2024-02-16 20:01:08 -0300

    chore: add chorme to docker

--- .github/actions/replay/entrypoint.sh
+++ .github/actions/replay/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-export PUPPETEER_CACHE_DIR=/root/.cache/puppeteer/
-
 read -d '' COMMAND <<-EOF
 	  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
 	    echo "$BUILD_DIR didn't change"
